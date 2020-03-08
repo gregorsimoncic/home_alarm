eec3270ff9970b22a014d6d47cd1c12a1f563aee
Gregor <gregorsimoncic@gmail.com>
2020-03-08 15:10:09 +0100

    Update stikalo.ino

--- stikalo/stikalo.ino
+++ stikalo/stikalo.ino
@@ -8,6 +8,10 @@
  *  ===============================
  */ 
 
+// common settings
+enum ALARM_STATE_ENUM { DISABLED, ARMED, TRIGGERED }; // status 0, 1, 2
+ALARM_STATE_ENUM alarm_state = DISABLED;
+
 // settings for switch
 const int sensor = 4; // sensor PIN number
 bool changed = false;
@@ -20,35 +24,85 @@ char received_chars_ard[num_chars]; // an array to store received data
 bool new_data = false;
 
 // settings for finite state machine
+bool alarm_state_change = false;
 
-
+// common setup
 void setup(){
   Serial.begin(9600);
   pinMode(sensor, INPUT_PULLUP);
 }
 
+// ========================
+//   MAIN LOOP
+// ========================
 void loop(){
+
+  // check alarm state and change it if need to
+  change_alarm_state();
   
   // read from switch
   read_switch();
   delay(10);
   
   // read from serial input
-  recv_with_end_marker();
-  show_me_data();
+  read_serial();
+  delay(10);
+  print_to_serial_from_serial_input();
+
+  // print status
+  print_status();
   
-  
+}
+// ========================
+//   CHANGE ALARM STATE
+// ========================
+void change_alarm_state() {
+  switch(alarm_state) {
+    case DISABLED:
+      if (strcmp(received_chars_ard, "ARM") == 0) {
+        alarm_state = ARMED;
+        Serial.println("Alarm is ARMED!");
+        alarm_state_change = true;
+        break;
+      }
+    alarm_state_change = false;
+    break;
+    case ARMED:
+      if (strcmp(received_chars_ard, "DISABLE") == 0) {
+        alarm_state = DISABLED;
+        Serial.println("Alarm is DISABLED!");
+        alarm_state_change = true;
+        break;
+      }
+      if ((strcmp(received_chars_ard, "TRIGGERED") == 0) or (switch_state == HIGH)) {
+        alarm_state = TRIGGERED;
+        Serial.println("Alarm is TRIGGERED!");
+        alarm_state_change = true;
+        break;
+      }
+    alarm_state_change = false;
+    break;
+    case TRIGGERED:
+      if (strcmp(received_chars_ard, "DISABLE") == 0) {
+        alarm_state = DISABLED;
+        Serial.println("Alarm is DISABLED!");
+        alarm_state_change = true;
+        break;
+      }
+    alarm_state_change = false;
+    break;
+  }
 }
 
 // ========================
 //   READ FROM SERIAL
 // ========================
-void recv_with_end_marker() {
+void read_serial() {
   static byte ndx = 0;
   char end_marker = '\n';
   char rc;
- 
- // if (Serial.available() > 0) {
+  
+  // if (Serial.available() > 0) {
   while (Serial.available() > 0 && new_data == false) {
     rc = Serial.read();
     
@@ -67,7 +121,10 @@ void recv_with_end_marker() {
   }
 }
 
-void show_me_data() {
+// ========================
+//   PRINT TO SERIAL FROM SERIAL READ
+// ========================
+void print_to_serial_from_serial_input() {
   if (new_data == true) {
     Serial.println(received_chars_ard);
     new_data = false;
@@ -112,17 +169,72 @@ void read_switch() {
 }
 
 // ========================
-//   SEND SMS TEST
+//  PRINT STATUS
 // ========================
+void print_status() {
+  if((strcmp(received_chars_ard, "STATUS") == 0)) {
+    switch(alarm_state) {
+    case 0:
+      Serial.println("STATUS is: DISABLED");
+      memset(received_chars_ard,0,num_chars);
+      break;
+    case 1:
+      Serial.println("STATUS is: ARMED");
+      memset(received_chars_ard,0,num_chars);
+      break;
+    case 2:
+      Serial.println("STATUS is: TRIGGERED");
+      memset(received_chars_ard,0,num_chars);
+      break;
+    }
+  }
+}
+
 // ========================
 //   SEND SMS STATUS
 // ========================
+void send_sms_status(){
+  
+}
+
 // ========================
-//   SEND SMS STOPED
+//   SEND SMS DISABLED
 // ========================
+void send_sms_disable(){
+  
+}
+
 // ========================
 //   SEND SMS ARMED
 // ========================
+void send_sms_armed(){
+  
+}
+
 // ========================
-//   SEND SMS TRIGERED
+//   SEND SMS TRIGGERED
 // ========================
+void send_sms_triggered(){
+  
+}
+
+// ========================
+//   RECEIVE SMS STATUS
+// ========================
+void receive_sms_status(){
+  
+}
+
+// ========================
+//   RECEIVE SMS DISABLE
+// ========================
+void receive_sms_disable(){
+  
+}
+
+// ========================
+//   RECEIVE SMS ARM
+// ========================
+void receive_sms_arm(){
+  
+}
